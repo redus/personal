6ab649fe2c79ed0f2cf97b32574747e6f36cb40f
Jin Yang <6439332+redus@users.noreply.github.com>
2025-11-16 15:41:38 -0800

    feat: a black box

---
+++ movement-0/index.js
@@ -0,0 +1,35 @@
+const canvas = document.querySelector("canvas");
+
+// Your WebGPU code will begin here!
+if (!navigator.gpu) {
+  throw new Error("WebGPU not supported on this browser, use WEBGL!");
+}
+
+const adapter = await navigator.gpu.requestAdapter();
+
+if (!adapter) {
+  throw new Error("No appropriate GPUAdapter found.");
+}
+const device = await adapter.requestDevice();
+
+// configure context
+const context = canvas.getContext("webgpu");
+const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
+context.configure({
+  device: device,
+  format: canvasFormat,
+});
+
+// encoder
+const encoder = device.createCommandEncoder();
+const pass = encoder.beginRenderPass({
+  colorAttachments: [{
+    view: context.getCurrentTexture().createView(),
+    loadOp: "clear",
+    storeOp: "store",
+  }]
+});
+pass.end();
+
+// issue
+device.queue.submit([encoder.finish()]);
